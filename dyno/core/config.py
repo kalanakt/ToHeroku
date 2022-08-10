92d2477d95430d465ce4ab562b2b1477be142dd6
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-08-10 21:35:51 +0530

    test

---
+++ dyno/core/config.py
@@ -0,0 +1,6 @@
+class Config(object):
+    FIRST_A_APIKEY = "09a7145c-7fb0-4605-a80d-71427da32505"
+    FIRST_B_APIKEY = "77128792-ac32-49b4-abf4-579eef7856c1"
+    FIRST_A_APPNAME = "testapp678"
+    FIRST_B_APPNAME = "codebluetesty"
+    FIRST_PROCESSTYPE = "worker"
--- dyno/core/main.py
+++ dyno/core/main.py
@@ -1,3 +1,47 @@
 import time
 from datetime import datetime
-import heroku3
+# import heroku3
+
+from config import Config
+
+FIRST_A_APIKEY = Config.FIRST_A_APIKEY
+FIRST_A_APPNAME = Config.FIRST_A_APPNAME
+FIRST_B_APPNAME = Config.FIRST_B_APPNAME
+FIRST_B_APIKEY = Config.FIRST_B_APIKEY
+FIRST_PROCESSTYPE = Config.FIRST_PROCESSTYPE
+
+print(FIRST_A_APIKEY, FIRST_A_APPNAME, FIRST_B_APPNAME,
+      FIRST_B_APIKEY, FIRST_PROCESSTYPE)
+# while True:
+#     today = datetime.now()
+#     print("[#1] Checking the conditions for the first app..")
+#     if len(FIRST_PROCESSTYPE) == 0 or len(FIRST_A_APIKEY) == 0 or len(
+#         FIRST_A_APPNAME) == 0 or len(FIRST_B_APIKEY) == 0 or len(
+#             FIRST_B_APPNAME) == 0:
+#         print("Variables for the first app are not fully filled.")
+#     elif (today.day == 15):
+#         print("Changing the dyno to the second acc..")
+#         heroku_conn = heroku3.from_key(FIRST_A_APIKEY)
+#         app = heroku_conn.app(FIRST_A_APPNAME)
+#         app.process_formation()[FIRST_PROCESSTYPE].scale(0)
+#         print("The first app in the first acc has been scaled down.")
+#         time.sleep(5)
+#         heroku_conn = heroku3.from_key(FIRST_B_APIKEY)
+#         app = heroku_conn.app(FIRST_B_APPNAME)
+#         app.process_formation()[FIRST_PROCESSTYPE].scale(1)
+#         print("The first app in the second acc has been scaled up.")
+#         print("Your first app has been shifted to the second acc.")
+#     elif(today.day == 10):
+#         print("Changing the dyno to the first acc..")
+#         heroku_conn = heroku3.from_key(FIRST_B_APIKEY)
+#         app = heroku_conn.app(FIRST_B_APPNAME)
+#         app.process_formation()[FIRST_PROCESSTYPE].scale(0)
+#         print("The first app in the second acc has been scaled down.")
+#         time.sleep(5)
+#         heroku_conn = heroku3.from_key(FIRST_A_APIKEY)
+#         app = heroku_conn.app(FIRST_A_APPNAME)
+#         app.process_formation()[FIRST_PROCESSTYPE].scale(1)
+#         print("The first app in the first acc has been scaled up.")
+#         print("Your first app has been shifted to the first acc.")
+#     else:
+#         print("Today is not 1st or 15nd.")
